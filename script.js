c3ac80fa991e0c6d675dbffd1cc96af166fd10b7
Durgesh Mishra <144038351+aditya1234-design@users.noreply.github.com>
2026-06-13 10:42:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function showMessage(){
+    alert('Thank you for supporting NayePankh Foundation!');
+}
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(current < target){
+            counter.innerText = Math.ceil(current + increment);
+            setTimeout(updateCounter, 20);
+        }else{
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+});
